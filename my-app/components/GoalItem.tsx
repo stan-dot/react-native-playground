064523aec00a26a9941555754ae7b3ab94c4b847
stan-dot <stanislawmalinowski09@gmail.com>
2023-01-23 23:04:27 +0000

    styling on pressable

--- my-app/components/GoalItem.tsx
+++ my-app/components/GoalItem.tsx
@@ -5,10 +5,16 @@ export type GoalItemType = {
   id: string;
 };
 
-export function GoalItem(props: { item: GoalItemType, onDeleteItem: (id: string) => void }) {
+export function GoalItem(
+  props: { item: GoalItemType; onDeleteItem: (id: string) => void },
+) {
   // this is wrapped as for iOS Text does not support rounded corners
   return (
-    <Pressable onPress={() => props.onDeleteItem(props.item.id)}>
+    <Pressable
+      onPress={() => props.onDeleteItem(props.item.id)}
+      android_ripple={{ color: "#5e0acc" }}
+      style={({ pressed }) => pressed && styles.pressedItem}
+    >
       <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.item.text}</Text>
       </View>
@@ -19,11 +25,16 @@ export function GoalItem(props: { item: GoalItemType, onDeleteItem: (id: string)
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+
+  pressedItem: {
+    opacity: 0.5,
+  },
+
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
