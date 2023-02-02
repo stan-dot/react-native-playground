3cafe8d0c2333afb93446bc2b0b1e196ba107a5d
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-02 23:11:00 +0000

    progress on home screen

--- react-native-course/deliveroo-clone/screens/HomeScreen.js
+++ react-native-course/deliveroo-clone/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import { useNavigation } from '@react-navigation/core';
 import { styled } from 'nativewind';
-import { SafeAreaView, Image, Text, View } from 'react-native';
+import { SafeAreaView, Image, Text, View, TextInput, ScrollView } from 'react-native';
 import { useLayoutEffect } from 'react';
-import { ChevronDownIcon, UserIcon, SearchIcon, AdjustmentsIcon, SparklesIcon as SparklesIconOutline } from 'react-native-heroicons/outline';
+import { ChevronDownIcon, UserIcon, SparklesIcon as SparklesIconOutline } from 'react-native-heroicons/outline';
+import { AdjustmentsVerticalIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 
 
 const StyledText = styled(Text)
@@ -16,22 +17,40 @@ export function HomeScreen() {
     });
   }, [])
 
-  return <View className="flex justify-center  " >
-
-    <SafeAreaView>
-      <Text className="text-red-500">Home screen</Text>
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+  return <SafeAreaView className="bg-white pt-5">
+    <Text className="text-red-500">
+      {/* Header*/}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-2">
         <Image source={{ url: 'https://links.papareact.com/wru' }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-        <View>
+        <View className={"flex-1"}>
           <SparklesIconOutline />
           <StyledText className="font-bold text-gray-400 text-xs">Deliver now!</StyledText>
           <StyledText className="font-bold text-xl">Current location
             <ChevronDownIcon size={20} color="#00CCBB" />
           </StyledText>
         </View>
+        <UserIcon size={35} color="#00CCBB" />
       </View>
-    </SafeAreaView>
-  </View >
+    </Text>
+
+    {/* Search */}
+    <View className="flex-row items-center space-x-2 pb-2 mx-4  px-4">
+      <View className="flex flex-row flex-1 space-x-2 bg-gray-200 p-3">
+        <MagnifyingGlassIcon color={'gray'} size={20} />
+        <TextInput placeholder='Restaurants and cuisines' keyboardType='default' />
+
+      </View>
+      <AdjustmentsVerticalIcon color="#00CCBB" />
+    </View>
+
+    {/* Body */}
+    <ScrollView>
+      {/* Categories */}
+      {/* Featured Rows*/}
+
+    </ScrollView>
+
+  </SafeAreaView>
 }
