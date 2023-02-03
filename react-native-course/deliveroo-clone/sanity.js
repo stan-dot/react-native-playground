2fbb6981f828f2cb5d41c0f33e0ac4df8be4c357
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-03 11:07:16 +0000

    client config

---
+++ react-native-course/deliveroo-clone/sanity.js
@@ -0,0 +1,17 @@
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+const client = sanityClient({
+  projectId: '4i58szf4',
+  dataset: "production",
+  useCdn: true,
+})
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
+// RUN THIS to addd execption for localhost 3000 cors policy
+// sanity cors add http://localhost:3000
+// or in the backend admin board
+
+export default client;
