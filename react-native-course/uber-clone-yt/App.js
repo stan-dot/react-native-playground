8c17c6ad46d7abd9719de3fc12dbb6b509b01a08
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-04 14:14:18 +0000

    canceled, dis not working

--- react-native-course/uber-clone-yt/App.js
+++ react-native-course/uber-clone-yt/App.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import HomeScreen from "./screens/HomeScreen";
 import { store } from './store';
+import "./tailwind.css";
 
 export default function App() {
   return (
--- react-native-course/uber-clone-yt/screens/HomeScreen.js
+++ react-native-course/uber-clone-yt/screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import "../tailwind.css";
+import tw from 'twrnc';
+import { styled } from 'tailwindcss';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <Text>I am the homescreen</Text>
+      <Text style={tw`text-cyan-400`}>I am the homescreen</Text>
     </SafeAreaView>
   )
 }
