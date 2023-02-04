a9bfa5687f890f959f371825c54da5b74d3b1366
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-04 13:47:14 +0000

    had to install some packages

---
+++ react-native-course/uber-clone-yt/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>I am the homescreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
