febc5553fe6dbbdfb347820044305f5391fd9492
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-05 09:58:18 +0000

    failed to setup tailwind

--- swipe-test/components/EditScreenInfo.tsx
+++ swipe-test/components/EditScreenInfo.tsx
@@ -1,19 +1,13 @@
 import * as WebBrowser from 'expo-web-browser';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 
 import Colors from '../constants/Colors';
-import { MonoText } from './StyledText';
-import { Text, View } from './Themed';
 
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
       <View style={styles.getStartedContainer}>
-        <Text
-          className="text-red-500"
-          // style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
+        <Text className="" >
           Open up the code for this screen:
         </Text>
 
