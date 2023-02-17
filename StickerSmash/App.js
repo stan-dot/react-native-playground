b394c6d86f26921a2c9853fbe31ff38d2cd6c00f
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-17 10:29:00 +0000

    first steps of the tutorial

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button'
+import * as ImagePicker from 'expo-image-picker';
+
+const PlaceholderImage = require("./assets/images/background-image.png");
+
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    });
+    if (!result.canceled) {
+      console.log(result);
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("you did not select any image")
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        {/* <Image source={PlaceholderImage} style={styles.image} /> */}
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label='Choose a photo' theme={'primary'} onPress={pickImageAsync} />
+        <Button label='Use this photo' />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2529e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
+  }
 });
