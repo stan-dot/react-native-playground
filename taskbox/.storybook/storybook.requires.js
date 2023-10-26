a19540e0ca565aecfaa9a79d34d66b9768855f7c
stan-dot <stanislawmalinowski09@gmail.com>
2023-10-26 10:57:35 +0100

    added the first story

--- taskbox/.storybook/storybook.requires.js
+++ taskbox/.storybook/storybook.requires.js
@@ -12,9 +12,9 @@ global.STORIES = [
   {
     titlePrefix: "",
     directory: "./components",
-    files: "**/*.stories.?(ts|tsx|js|jsx)",
+    files: "**/*.stories.@(ts|tsx|js|jsx)",
     importPathMatcher:
-      "^\\.[\\\\/](?:components(?:\\/(?!\\.)(?:(?:(?!(?:^|\\/)\\.).)*?)\\/|\\/|$)(?!\\.)(?=.)[^/]*?\\.stories\\.(?:ts|tsx|js|jsx)?)$",
+      "^\\.[\\\\/](?:components(?:\\/(?!\\.)(?:(?:(?!(?:^|\\/)\\.).)*?)\\/|\\/|$)(?!\\.)(?=.)[^/]*?\\.stories\\.(ts|tsx|js|jsx))$",
   },
 ];
 
@@ -46,7 +46,9 @@ try {
 } catch {}
 
 const getStories = () => {
-  return {};
+  return {
+    "./components/Task.stories.jsx": require("../components/Task.stories.jsx"),
+  };
 };
 
 configure(getStories, module, false);
